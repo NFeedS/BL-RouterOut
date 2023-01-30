21e310a8a7c29ce4983af74f326bd186a392cf67
Leonardo Malcotti <leonardo.malcotti@studenti.unitn.it>
2023-01-30 15:29:31 +0100

    spring init

---
+++ src/main/java/com/nfeeds/business/routerout/RouteroutApplication.java
@@ -0,0 +1,13 @@
+package com.nfeeds.business.routerout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteroutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteroutApplication.class, args);
+	}
+
+}
